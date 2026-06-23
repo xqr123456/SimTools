cd0951b4e75a442b60f123d1565aede296590c71
qunren xu <57185488+xqr123456@users.noreply.github.com>
2026-06-23 08:53:30 +0800

    Add files via upload

--- sl_customization.m
+++ sl_customization.m
@@ -18,7 +18,7 @@ function schema = simtools_container(~)
 
     schema = sl_container_schema;
     schema.label = '🚀 SimTools';
-    schema.childrenFcns = {@menu_mapping, @menu_io, @menu_app};
+    schema.childrenFcns = {@menu_mapping, @menu_io, @menu_app, @menu_Subsystem_addPort, @menu_Constant_to_goto, @menu_Inport_to_goto};
 
 end
 
@@ -49,5 +49,32 @@ function schema = menu_app(~)
     schema = sl_action_schema;
     schema.label = '打开 SimTools App';
     schema.callback = @(cb) simtools_openMainApp();
+
+end
+
+%% 子菜单4
+function schema = menu_Subsystem_addPort(~)
+
+    schema = sl_action_schema;
+    schema.label = 'Subsystem_addPort';
+    schema.callback = @(cb) simtools_Subsystem_addPort();
+
+end
+
+%% 子菜单5
+function schema = menu_Constant_to_goto(~)
+
+    schema = sl_action_schema;
+    schema.label = 'Constant_to_goto';
+    schema.callback = @(cb) simtools_Constant_to_goto();
+
+end
+
+%% 子菜单6
+function schema = menu_Inport_to_goto(~)
+
+    schema = sl_action_schema;
+    schema.label = 'Inport_to_goto';
+    schema.callback = @(cb) simtools_Inport_to_goto();
 
 end
